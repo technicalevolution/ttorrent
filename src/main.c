8793b47b8caf8b9ba920a06fcfc106db6a914001
techevo <technicalevolution@gmail.com>
2020-03-06 21:41:12 +0000

    opt args working, and making sure that the input file exists

--- src/main.c
+++ src/main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
 
 void usage( void );
 
 int main( int argc, char **argv, char **env ) {
 
-  char *file;
-  char *output;
+  char *file = NULL;
+  char *output = NULL;
   
   int c;
+
+  struct stat *statbuf;
 
   while ((c = getopt (argc, argv, "f:o:")) != -1)
     switch (c)
@@ -26,7 +32,27 @@ int main( int argc, char **argv, char **env ) {
           usage();
       }
 
+  if( file == NULL ) {
+    usage();
+    return 1;
+  } else if ( output == NULL ) {
+    usage();
+    return 1;
+  }
 
+  statbuf = (struct stat *)calloc(1, sizeof(struct stat)); 
+  if(stat(file, statbuf) != 0) {
+    printf("There was a problem acessing the file: %s\n", file);
+    return 1;
+  }
+  free(statbuf);
+
+  
+
+  
+  
+  
+  
   return 0;
 
 }
